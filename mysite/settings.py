3a3e8f627e5cb92e28d1f485f522c5d1e7ec32fa
abelmoricz-evo <102213273+abelmoricz-evo@users.noreply.github.com>
2022-12-05 16:41:35 +0100

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'POST_collector',
+    'standardized_view',
+
 ]
 
 MIDDLEWARE = [
